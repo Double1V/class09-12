49d9bd9a725e766eb69e0d163853b8c3f63e9159
Double1V <113297720+Double1V@users.noreply.github.com>
2022-12-09 14:20:43 +0300

    Solution

---
+++ Solution.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    static int stray(int[] numbers) {
+        Arrays.sort(numbers);
+        if (numbers[0] == numbers[1]) {
+            return numbers[numbers.length - 1];
+        }
+        return numbers[0];
+    }
+}
